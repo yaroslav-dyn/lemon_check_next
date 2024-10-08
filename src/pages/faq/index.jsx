62edb66da9136319d896461eee0c007ffd8834e4
Yaroslav Kurilin <yaroslav.kurilin.wedev@gmail.com>
2024-10-08 13:41:07 +0300

    Add PWA.

--- src/pages/faq/index.jsx
+++ src/pages/faq/index.jsx
@@ -10,7 +10,7 @@ const AboutPage = () => {
         <meta name="description" content="About LemonCheck" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,9 +9,10 @@ export default function Home() {
       <Head>
         <title>LemonCheck</title>
         <meta name="description" content="LemonCheck" />
+
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
