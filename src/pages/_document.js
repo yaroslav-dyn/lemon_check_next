06a337258c66ea20766d795e19dcedbdaf425978
Yaroslav Kurilin <yaroslav.kurilin.wedev@gmail.com>
2025-06-09 16:34:37 +0300

    Add ads script (test).

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -22,6 +22,10 @@ export default function Document() {
           rel="stylesheet"
         />
         <link rel="manifest" href="/manifest.json" />
+
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1265222607784542"
+          crossorigin="anonymous"></script>
+
       </Head>
       <body>
         <Main />
